bd30c2290661ddc0240eb37356cf3e3e5111a6e2
DGWerlod <ddeanda333@gmail.com>
2019-10-29 13:58:12 -0500

    Made things pretty to prep for commenting

--- src/magicSquaresPackage/MagicSquares.java
+++ src/magicSquaresPackage/MagicSquares.java
@@ -1,193 +1,14 @@
 package magicSquaresPackage;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
-import java.util.Vector;
 
 @SuppressWarnings("WeakerAccess")
 public class MagicSquares {
 
-    private Scanner file = null;
+    // This constant ensures that the file path on line xx is platform-agnostic
+    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
 
-    private Vector<Integer> numElements;
-    private Vector<Vector<Integer>> magicSquare;
-    private int magicSquareSum;
-
-    public MagicSquares() {
-        numElements = new Vector<>();
-        magicSquare = new Vector<>();
-        magicSquareSum = 0;
-    }
-
-    public int getCurrentSquareSize() {
-        return magicSquare.size();
-    }
-
-    public int getCurrentMagicSum() {
-        return magicSquareSum;
-    }
-
-    public boolean openFile(String path) {
-        try {
-            FileReader reader = new FileReader(path);
-            file = new Scanner(reader);
-        } catch (FileNotFoundException e) { // IOException?
-            System.out.println("ERROR: File " + path + " did not open successfully.");
-            return false;
-        }
-        return true;
-    }
-
-    public boolean loadNextSquare() {
-        if (file.hasNextLine()) {
-
-            int size = Integer.parseInt(file.nextLine());
-            magicSquare.clear();
-            magicSquare.setSize(size);
-            magicSquareSum = (size*(size*size+1))/2;
-
-            for (int row = 0; row < size; row++) {
-
-                magicSquare.set(row, new Vector<>());
-                magicSquare.get(row).setSize(size);
-                String[] currentRow = file.nextLine().split(" ");
-
-                for (int column = 0; column < size; column++) {
-                    int currentValue = Integer.parseInt(currentRow[column]);
-                    magicSquare.get(row).set(column, currentValue);
-                }
-
-            }
-
-            return false;
-
-        }
-        file.close();
-        return true;
-    }
-
-    public void printCurrentSquare() {
-
-        for (int row = 0; row < magicSquare.size(); row++) {
-
-            StringBuilder output = new StringBuilder(); // StringBuilders instead of Strings for efficiency
-
-            for (int column = 0; column < magicSquare.size(); column++) {
-
-                // Formats each number to have three digits (based on in-class direction and sample output)
-                StringBuilder currentNumber = new StringBuilder(String.valueOf(magicSquare.get(row).get(column)));
-                while (currentNumber.length() < 3) {
-                    currentNumber.insert(0, "0");
-                }
-                output.append(currentNumber).append(" ");
-
-            }
-
-            // removes final trailing ' ' in output
-            System.out.println(output.substring(0, output.length()-1));
-
-        }
-
-    }
-
-    private boolean checkElements() {
-
-        int size = magicSquare.size();
-        numElements.clear();
-        numElements.setSize(size*size+1); // elements range from 1-25; ignoring index 0
-        for (int i = 1; i < numElements.size(); i++) {
-            numElements.set(i, 0);
-        }
-
-        boolean isValid = true;
-
-        for (int row = 0; row < magicSquare.size(); row++) {
-            for (int column = 0; column < magicSquare.size(); column++) {
-
-                int now = magicSquare.get(row).get(column);
-                if (now >= 1 && now <= numElements.size()) {
-                    numElements.set(now, numElements.get(now)+1);
-                } else {
-                    isValid = false;
-                    System.out.print("The square has an entry " + now + " that is greater than ");
-                    System.out.println(size + " squared which is " + (numElements.size()-1));
-                    // subtract 1 because we don't use zero
-                }
-
-            }
-        }
-
-        for (int i = 1; i < numElements.size(); i++) {
-            if (numElements.get(i) == 0) { // only possibility for less than 1
-                isValid = false;
-                System.out.println("The square does not have a " + i);
-            } else if (numElements.get(i) > 1) {
-                isValid = false;
-                System.out.print("The square is not unique, it has ");
-                System.out.println(numElements.get(i) + "elements with values of " + i);
-            }
-        }
-
-        return isValid;
-
-    }
-
-    private boolean checkRowsAndColumns() {
-        int rowSum, columnSum;
-        boolean isValid = true;
-        for (int i = 0; i < magicSquare.size(); i++) {
-            rowSum = 0;
-            columnSum = 0;
-            for (int j = 0; j < magicSquare.size(); j++) {
-                rowSum += magicSquare.get(i).get(j); // row = i, column = j
-                columnSum += magicSquare.get(j).get(i); // column = i, row = j
-            }
-            if (rowSum != magicSquareSum) {
-                isValid = false;
-                System.out.print("Row #" + (i+1) + " does not produce the expected magic square sum ");
-                System.out.println("for a " + magicSquare.size() + " magic square (" + magicSquareSum + ")");
-            }
-            if (columnSum != magicSquareSum) {
-                isValid = false;
-                System.out.print("Column #" + (i+1) + " does not produce the expected magic square sum ");
-                System.out.println("for a " + magicSquare.size() + " magic square (" + magicSquareSum + ")");
-            }
-        }
-        return isValid;
-    }
-
-    private boolean checkDiagonals() {
-        int forwardSum = 0;
-        int backwardSum = 0;
-        boolean isValid = true;
-        for (int i = 0; i < magicSquare.size(); i++) {
-            forwardSum += magicSquare.get(i).get(i); // i is the row and column number
-            int backwardIndex = magicSquare.size()-i-1; // inverting the row and column number
-            backwardSum += magicSquare.get(backwardIndex).get(backwardIndex);
-        }
-        if (forwardSum != magicSquareSum) {
-            isValid = false;
-            System.out.print("The forward diagonal does not produce the expected magic square sum ");
-            System.out.println("for a " + magicSquare.size() + " magic square (" + magicSquareSum + ")");
-        }
-        if (backwardSum != magicSquareSum) {
-            isValid = false;
-            System.out.print("The backward diagonal does not produce the expected magic square sum ");
-            System.out.println("for a " + magicSquare.size() + " magic square (" + magicSquareSum + ")");
-        }
-        return isValid;
-    }
-
-    public boolean isMagicSquare() {
-        if (this.checkElements()) {
-            if (this.checkRowsAndColumns()) {
-                return this.checkDiagonals();
-            }
-        }
-        return false;
-    }
-
+    // This Scanner exists specifically to listen for user input before continuing
     public static Scanner keyboard = new Scanner(System.in);
 
     public static void waitForUserInput() {
@@ -198,11 +19,12 @@ public class MagicSquares {
 
     public static void main(String[] args) {
 
-        MagicSquares validator = new MagicSquares();
+        MagicSquareValidator validator = new MagicSquareValidator();
         String separator = "--------------------------------------------"; // Indicates a new "screen"
 
-        boolean successful = validator.openFile("src/magicSquaresPackage/magic.txt");
-        if (successful) {
+        String path = "src" + FILE_SEPARATOR + "magicSquaresPackage" + FILE_SEPARATOR + "magic.txt";
+        boolean openSuccessful = validator.openFile(path);
+        if (openSuccessful) {
 
             do {
 
